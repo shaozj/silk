0d11ae568f597a02c755603515c1d94f9f170f57
澜嵚 <814184517@qq.com>
2017-11-08 15:20:47 +0800

    up

--- src/babel.js
+++ src/babel.js
@@ -1,17 +1,11 @@
 'use strict';
 
-import babel from 'babel-core';
-import getPaths from './utils/paths';
-import { resolve } from 'path';
-import fs from 'fs';
 import 'shelljs/global';
 
 const cwd = process.cwd();
-const paths = getPaths(cwd);
 
 export default function babelCompile(config) {
   const isProduction = process.env.NODE_ENV === 'production';
-  console.log('NODE_ENV', process.env.NODE_ENV);
   // const outputPath = argv.outputPath || config.outputPath || 'build';
   // let outDir = paths.resolveApp(outputPath);
   // 默认的输入输出地址
