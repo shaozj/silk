61836177ee04b0ad9b3053115ff640984cca4592
澜嵚 <814184517@qq.com>
2017-12-05 16:05:29 +0800

    up

--- src/generator/generators/mod/youku-mod-template/src/index.js
+++ src/generator/generators/mod/youku-mod-template/src/index.js
@@ -1,38 +1,57 @@
 // 我是组件代码主体文件，在构建组件线上版本时，我是入口文件
 
 import './index.less';
-import {YKComponent,YKItem, YKTag, YKImg, YKSummary, YKSubTitle, YKTitle, Base} from '@ali/yk-base';
+import {
+  YKComponent,
+  YKItem,
+  YKTag,
+  YKImg,
+  YKSummary,
+  YKSubTitle,
+  YKTitle,
+  Base
+} from '@ali/yk-base';
+
 class App extends Base {
   constructor(props) {
     super(props);
-    this.state = {...props};
+    this.state = { ...props };
   }
+
   render() {
-    const {data = {componentItem: [], type: ''}, coverLay, env} = this.state;
-    const items =  (data && data.componentItem)||[];
+    const {
+      data = { componentItem: [], type: '' },
+      coverLay,
+      env
+    } = this.state;
+    const items = (data && data.componentItem) || [];
+
     return (
-      <YKComponent {...data}
-        className = "ykui-two-items"
-        dataType = {data && data.type}
-        env = {env}
-        addItem={::this.addItem}>
-          {
-            items.map((item,index)=>{
-              return (
-                <YKItem href="#" className="ui-item item" coverLay={coverLay} env={env} coverLayPos={index} key={index} data={item}>
-                  <div className="ui-bg">
-                    <YKImg data={item} />
-                    <YKSummary data={item} />
-                    <YKTag data={item} />
-                  </div>
-                  <div className="ui-info">
-                    <YKTitle data={item} />
-                    <YKSubTitle data={item} />
-                  </div>
-                </YKItem>
-              );
-          })}
-      </YKComponent>
+      <YKComponent
+        {...data}
+        className="ykui-two-items"
+        dataType={data && data.type}
+        env={env}
+        addItem={:: this.addItem}
+      >
+  {
+    items.map((item, index) => {
+      return (
+        <YKItem href="#" className="ui-item item" coverLay={coverLay} env={env} coverLayPos={index} key={index} data={item}>
+          <div className="ui-bg">
+            <YKImg data={item} />
+            <YKSummary data={item} />
+            <YKTag data={item} />
+          </div>
+          <div className="ui-info">
+            <YKTitle data={item} />
+            <YKSubTitle data={item} />
+          </div>
+        </YKItem>
+      );
+    })
+  }
+      </YKComponent >
     );
   }
 }
@@ -41,4 +60,3 @@ export default App;
 window.YKComponent = window.YKComponent || {};
 window.YKComponent.ykui = window.YKComponent.ykui || {};
 window.YKComponent.ykui['ykui-two-items'] = App;
-
--- src/generator/generators/mod/youku-mod-template/src/mock/data.js
+++ src/generator/generators/mod/youku-mod-template/src/mock/data.js
@@ -1,64 +1,115 @@
+/* eslint-disable quotes */
+// 一个组件的 mock 数据
+
 window.mockData = {
-	line: 1,
-	hasTail: 0,
-	tailText: "",
-	tailTextBizType: "",
-	tailTextExtra: null,
-	tailChangeText: "",
-	tailChangeNum: 3,
-	tailChangeType: 0,
-	tailExpandText: "",
-	tailExpandLine: 0,
-	componentItem:[
-		{
-			"summary": "这是腰封",
-			"title": "这是主标题",
-			"subtitleType": "GENERAL",
-			"img": "http://ykimg.alicdn.com/product/image/2017-05-11/189eb75f9143bd9c6d99556afe5702b6.jpg?x-oss-process=image/resize,p_100",
-			"subtitle": "这是副标题",
-			"gifImg": "",
-			"type": "GENERAL",
-			"summaryType": "GENERAL"
-		},
-		{
-			"summary": "这是腰封",
-			"title": "这是主标题",
-			"subtitleType": "GENERAL",
-			"img": "http://ykimg.alicdn.com/product/image/2017-05-11/189eb75f9143bd9c6d99556afe5702b6.jpg?x-oss-process=image/resize,p_100",
-			"subtitle": "这是副标题",
-			"gifImg": "",
-			"type": "GENERAL",
-			"summaryType": "GENERAL"
-		},
-		{
-			"summary": "这是腰封",
-			"title": "这是主标题",
-			"subtitleType": "GENERAL",
-			"img": "http://ykimg.alicdn.com/product/image/2017-05-11/189eb75f9143bd9c6d99556afe5702b6.jpg?x-oss-process=image/resize,p_100",
-			"subtitle": "这是副标题",
-			"gifImg": "",
-			"type": "GENERAL",
-			"summaryType": "GENERAL"
-		},
-		{
-			"summary": "这是腰封",
-			"title": "这是主标题",
-			"subtitleType": "GENERAL",
-			"img": "http://ykimg.alicdn.com/product/image/2017-05-11/189eb75f9143bd9c6d99556afe5702b6.jpg?x-oss-process=image/resize,p_100",
-			"subtitle": "这是副标题",
-			"gifImg": "",
-			"type": "GENERAL",
-			"summaryType": "GENERAL"
-		},
-		{
-			"summary": "这是腰封",
-			"title": "这是主标题",
-			"subtitleType": "GENERAL",
-			"img": "http://ykimg.alicdn.com/product/image/2017-05-11/189eb75f9143bd9c6d99556afe5702b6.jpg?x-oss-process=image/resize,p_100",
-			"subtitle": "这是副标题",
-			"gifImg": "",
-			"type": "GENERAL",
-			"summaryType": "GENERAL"
-		}
-	]
+  "line": 1,
+  "hasTail": 0,
+  "tailText": "",
+  "tailTextBizType": "",
+  "tailTextExtra": null,
+  "tailChangeText": "",
+  "tailChangeNum": 3,
+  "tailChangeType": 0,
+  "tailExpandText": "",
+  "tailExpandLine": 0,
+  "componentItem": [
+    {
+      "summary": "这是腰封",
+      "title": "这是主标题",
+      "subtitleType": "GENERAL",
+      "img": "http://ykimg.alicdn.com/product/image/2017-05-11/189eb75f9143bd9c6d99556afe5702b6.jpg?x-oss-process=image/resize,p_100",
+      "subtitle": "这是副标题",
+      "gifImg": "",
+      "type": "GENERAL",
+      "summaryType": "GENERAL",
+      "action": {
+        "extra": {},
+        "type": ""
+      },
+      "h5Action": {
+        "url": ""
+      },
+      "property": {}
+    },
+    {
+      "summary": "这是腰封",
+      "title": "这是主标题",
+      "subtitleType": "GENERAL",
+      "img": "http://ykimg.alicdn.com/product/image/2017-05-11/189eb75f9143bd9c6d99556afe5702b6.jpg?x-oss-process=image/resize,p_100",
+      "subtitle": "这是副标题",
+      "gifImg": "",
+      "type": "GENERAL",
+      "summaryType": "GENERAL",
+      "action": {
+        "extra": {},
+        "type": ""
+      },
+      "h5Action": {
+        "url": ""
+      },
+      "property": {}
+    },
+    {
+      "summary": "这是腰封",
+      "title": "这是主标题",
+      "subtitleType": "GENERAL",
+      "img": "http://ykimg.alicdn.com/product/image/2017-05-11/189eb75f9143bd9c6d99556afe5702b6.jpg?x-oss-process=image/resize,p_100",
+      "subtitle": "这是副标题",
+      "gifImg": "",
+      "type": "GENERAL",
+      "summaryType": "GENERAL",
+      "action": {
+        "extra": {},
+        "type": ""
+      },
+      "h5Action": {
+        "url": ""
+      },
+      "property": {}
+    },
+    {
+      "summary": "这是腰封",
+      "title": "这是主标题",
+      "subtitleType": "GENERAL",
+      "img": "http://ykimg.alicdn.com/product/image/2017-05-11/189eb75f9143bd9c6d99556afe5702b6.jpg?x-oss-process=image/resize,p_100",
+      "subtitle": "这是副标题",
+      "gifImg": "",
+      "type": "GENERAL",
+      "summaryType": "GENERAL",
+      "action": {
+        "extra": {},
+        "type": ""
+      },
+      "h5Action": {
+        "url": ""
+      },
+      "property": {}
+    },
+    {
+      "summary": "这是腰封",
+      "title": "这是主标题",
+      "subtitleType": "GENERAL",
+      "img": "http://ykimg.alicdn.com/product/image/2017-05-11/189eb75f9143bd9c6d99556afe5702b6.jpg?x-oss-process=image/resize,p_100",
+      "subtitle": "这是副标题",
+      "gifImg": "",
+      "type": "GENERAL",
+      "summaryType": "GENERAL",
+      "action": {
+        "extra": {},
+        "type": ""
+      },
+      "h5Action": {
+        "url": ""
+      },
+      "property": {}
+    }
+  ],
+  "styleConfigObject": {
+    "bgType": 0,
+    "bgColor": "",
+    "bgImg": "",
+    "titleColor": "",
+    "subTitleColor": ""
+  },
+  "dynamicExtendObejct": {}
 };
