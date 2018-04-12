708700e7c53457623694ad3cc5683cac9f8adc5e
澜嵚 <814184517@qq.com>
2018-04-12 10:35:40 +0800

    fix: import React from React

--- src/generator/generators/app/spa-template/src/components/Layout/LeftNav.js
+++ src/generator/generators/app/spa-template/src/components/Layout/LeftNav.js
@@ -1,5 +1,3 @@
-import React from 'React';
-
 function LeftNav({ children, curTop, curLeft }){
   const curLeftNav = children && children.find((child) => {
     return child.props.parentKey == curTop;
