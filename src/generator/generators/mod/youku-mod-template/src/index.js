ba8a12fd13f949e3d617666ad05f84505720cbfc
澜嵚 <814184517@qq.com>
2018-05-14 17:51:49 +0800

    new mod template

--- src/generator/generators/mod/youku-mod-template/src/index.js
+++ src/generator/generators/mod/youku-mod-template/src/index.js
@@ -1,16 +1,14 @@
 // 我是组件代码主体文件，在构建组件线上版本时，我是入口文件
 
 import './index.less';
-import {
-  YKComponent,
-  YKItem,
-  YKTag,
-  YKImg,
-  YKSummary,
-  YKSubTitle,
-  YKTitle,
-  Base
-} from '@ali/yk-base';
+const YKComponent = window.YKComponent.ykui.YKComponent;
+const YKItem = window.YKComponent.ykui.YKItem;
+const YKTag = window.YKComponent.ykui.YKTag;
+const YKImg = window.YKComponent.ykui.YKImg;
+const YKSummary = window.YKComponent.ykui.YKSummary;
+const YKSubTitle = window.YKComponent.ykui.YKSubTitle;
+const YKTitle = window.YKComponent.ykui.YKTitle;
+const Base = window.YKComponent.ykui.Base;
 
 class App extends Base {
   constructor(props) {
@@ -32,25 +30,25 @@ class App extends Base {
         className="ykui-two-items"
         dataType={data && data.type}
         env={env}
-        addItem={:: this.addItem}
+        addItem={::this.addItem}
       >
-  {
-    items.map((item, index) => {
-      return (
-        <YKItem href="#" className="ui-item item" coverLay={coverLay} env={env} coverLayPos={index} key={index} data={item}>
-          <div className="ui-bg">
-            <YKImg data={item} />
-            <YKSummary data={item} />
-            <YKTag data={item} />
-          </div>
-          <div className="ui-info">
-            <YKTitle data={item} />
-            <YKSubTitle data={item} />
-          </div>
-        </YKItem>
-      );
-    })
-  }
+      {
+        items.map((item, index) => {
+          return (
+            <YKItem href="#" className="ui-item item" coverLay={coverLay} env={env} coverLayPos={index} key={index} data={item}>
+              <div className="ui-bg">
+                <YKImg data={item} />
+                <YKSummary data={item} />
+                <YKTag data={item} />
+              </div>
+              <div className="ui-info">
+                <YKTitle data={item} />
+                <YKSubTitle data={item} />
+              </div>
+            </YKItem>
+          );
+        })
+      }
       </YKComponent >
     );
   }
